bc4bf16410c5c8e5a6edcb681bbafbb0a506e024
Isha Bule <ishabule753@gmail.com>
2023-10-01 22:13:52 +0530

    CSV created

--- Bangalore.py
+++ Bangalore.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv # To store the data in a CSV file
 
 # URL of Swiggy's restaurant page
 url = 'https://www.swiggy.com/city/bangalore/best-restaurants'
@@ -13,7 +14,10 @@ response = requests.get(url, headers=headers)
 
 print(f"Status Code: {response.status_code}")
 
-
+# open or create a csv file
+csv_file = open('Bangalore.csv', 'w')
+writer = csv.writer(csv_file)
+writer.writerow(['Restaurant Name', 'Rating', 'Cuisine', 'Location'])
 
 # Check if the request was successful
 if response.status_code == 200:
@@ -36,15 +40,11 @@ if response.status_code == 200:
 
         print(f'Restaurant Name: {restaurant_name}')
         print(f'Rating: {rating}')
+        print(f'Cuisine: {data[0].text.strip()}')
+        print(f'Location: {data[1].text.strip()}')
         
-        count = 0
-        for i in data:
-          if(count == 0):
-            print('Cuisine: ', i.text.strip())
-          if(count == 1):
-            print('Location: ', i.text.strip())
-          
-          count += 1
+        # write the data to csv file
+        writer.writerow([restaurant_name, rating, data[0].text.strip(), data[1].text.strip()])
         
        
    
